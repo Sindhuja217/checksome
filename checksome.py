51f027762f1533226d6a135bca55c7b70d9cfa18
Sindhuja217 <107912857+Sindhuja217@users.noreply.github.com>
2022-06-27 11:26:43 +0530

    Add files via upload

---
+++ checksome.py
@@ -0,0 +1,13 @@
+import sayany
+
+def doit():
+    sayany.hello()
+
+def add(a,b):
+    print('I am in checksome...')
+    c = a + b
+    return c
+w = [1, 9, 4]
+
+def sub():
+    print('I am in checksome. Yahoo!')
